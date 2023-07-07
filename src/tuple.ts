a91eb0a4f060b7a08ef134bdf21908ecde868107
Mark Lester Acak <mister6virgo@gmail.com>
2023-07-07 14:39:29 +0800

    Second Version

---
+++ src/tuple.ts
@@ -0,0 +1,9 @@
+// type Drink = [string, boolean, number];
+
+// const pepsi: Drink = ['brown', true, 40];
+
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// pepsi[0] = 123;
+
+const pepsi = ['brown', true, 40];
